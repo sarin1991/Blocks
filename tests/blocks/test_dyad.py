278467775a209b7f5d7b3874658e1c0910269f62
Sarin Eapen Chandy <sarin1991@gmail.com>
2023-07-28 11:05:44 -0500

    Adding sequential dyad test

--- tests/blocks/test_dyad.py
+++ tests/blocks/test_dyad.py
@@ -104,3 +104,49 @@ def test_non_symmetric_out_dyad(has_bias,val):
     if has_bias:
         assert torch.allclose(lay1.bias.grad,lay2.bias.grad)
     assert torch.allclose(x1.grad,x2.grad)
+
+@pytest.mark.parametrize("has_bias",[True, False])
+@pytest.mark.parametrize("val",[-2.1,1,3,5.4])
+def test_sequential_dyad(has_bias,val):
+    dim_in = 2
+    dim_out = 2
+    dyad_dim = 2
+    batch_size = 1
+    lay1 = torch.nn.Sequential(
+        DyadBlock(dyad_dim,dim_in,dim_out,has_bias),
+        DyadBlock(dyad_dim,dim_in,dim_out,has_bias),
+        DyadBlock(dyad_dim,dim_in,dim_out,has_bias)
+    )
+    lay2 = torch.nn.Sequential(
+        PyDyad(dyad_dim,dim_in,dim_out,has_bias),
+        PyDyad(dyad_dim,dim_in,dim_out,has_bias),
+        PyDyad(dyad_dim,dim_in,dim_out,has_bias)
+    )
+    # init weights
+    for i in range(3):
+        lay1[i].w_upper = torch.nn.Parameter((val+12)*torch.ones_like(lay1[i].w_upper.data))
+        lay1[i].w_lower = torch.nn.Parameter(val*torch.ones_like(lay1[i].w_lower.data))
+        expected = torch.arange(0,6,dtype=torch.float32)
+        if has_bias:
+            lay1[i].bias = torch.nn.Parameter(torch.ones_like(lay1[i].bias.data))
+            b = lay1[i].bias.data
+        else:
+            lay1[i].bias = None
+            b = None
+        lay2[i].from_weights(lay1[i].w_upper.data,lay1[i].w_lower.data,b)
+    x1 = torch.rand((dyad_dim*dim_in,batch_size),dtype=torch.float32)
+    x1.requires_grad = True
+    x2 = x1.detach().clone()
+    x2.requires_grad = True
+    out1 = lay1(x1)
+    out2 = lay2(x2)
+    print(out1,out2)
+    assert torch.allclose(out1,out2)
+    (out1-expected).sum().backward()
+    (out2-expected).sum().backward()
+    for i in range(3):
+        assert torch.allclose(lay1[i].w_upper.grad,lay2[i].w_upper.grad)
+        assert torch.allclose(lay1[i].w_lower.grad,lay2[i].w_lower.grad)
+        if has_bias:
+            assert torch.allclose(lay1[i].bias.grad,lay2[i].bias.grad)
+    assert torch.allclose(x1.grad,x2.grad)
